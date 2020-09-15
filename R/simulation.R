a48ad3e1dedabe2e5433fac69607514d1dc5293c
dalejbarr <dalejbarr@protonmail.com>
2020-09-15 21:44:38 +0100

    slight modification to sim_2x2 interface, tests for item effects

--- R/simulation.R
+++ R/simulation.R
@@ -175,23 +175,19 @@ errsim <- function(n_obs, version) {
 #' }
 #'
 #' @export
-sim_2x2 <- function(n_subj = 48, n_obs = 48, n_items = 48,
+sim_2x2 <- function(n_subj = 48, n_items = 48,
                     int = 0, A = 0, B = 0, AB = 0,
                     item_rint = .5, item_rslp = .5, item_rcorr = .5,
                     subj_rint = .5, subj_rslp = .5, subj_rcorr = .5,
-                    version = 0L, verbose = FALSE){
+                    version = 0L, verbose = FALSE) {
 
+  # n_obs <- n_items # for future development
+  
   if ((n_subj %% 4L) || (n_subj <= 0L))
     stop("'n_subj' must be a positive integer that is a multiple of 4")
 
-  if ((n_obs %% 2L) || (n_obs <= 0L))
-    stop("'n_obs' must be a positive even integer")
-
   if ((n_items %% 2L) || (n_items <= 0L))
     stop("'n_items' must be a positive even integer")
-
-  if (n_obs %% n_items)
-    warning("Are you sure 'n_items' shouldn't be a factor of 'n_obs'?")
 
   make_rfx <- function(rint, rslp, rcorr){
     rfx_mx <- matrix(c(rint,
@@ -199,21 +195,11 @@ sim_2x2 <- function(n_subj = 48, n_obs = 48, n_items = 48,
                        sqrt(rint) * sqrt(rslp) * rcorr,
                        rslp), nrow = 2)
 
-    
-    ## rfx <- as.data.frame(
-    ##   MASS::mvrnorm(n,
-    ##                 c(rint = 0, rslp = 0),
-    ##                 rfx_mx))
-
     return(rfx_mx)
   }  
 
   subj_rfx_mx <- make_rfx(subj_rint, subj_rslp, subj_rcorr)
   item_rfx_mx <- make_rfx(item_rint, item_rslp, item_rcorr)
-
-  ## sfx <- make_rfx(n_subj, subj_rint, subj_rslp, subj_rcorr)
-  ## ifx <- make_rfx(n_items, item_rint, item_rslp, item_rcorr)
-
   
   sfx <- as.data.frame(
     MASS::mvrnorm(n_subj,
@@ -255,18 +241,18 @@ sim_2x2 <- function(n_subj = 48, n_obs = 48, n_items = 48,
 
   ds <- split(dat, dat[["subj_id"]])
 
-  errs <- replicate(n_subj, errsim(n_obs, version), simplify = FALSE)
+  errs <- replicate(n_subj, errsim(n_items, version), simplify = FALSE)
 
   ## add in the errors
   derr <- mapply(function(.d, .e) {
     .d[["tnum_r"]] <- sample(seq_len(nrow(.d)))
     .d2 <- split(.d, .d[["A"]])
     if (.d[["blk_ord"]][1] == 1L) {
-      .d2[[1]][["tnum_b"]] <- sample(seq_len(n_obs / 2L))
-      .d2[[2]][["tnum_b"]] <- sample(seq_len(n_obs / 2L)) + (n_obs / 2L)
+      .d2[[1]][["tnum_b"]] <- sample(seq_len(n_items / 2L))
+      .d2[[2]][["tnum_b"]] <- sample(seq_len(n_items / 2L)) + (n_items / 2L)
     } else {
-      .d2[[1]][["tnum_b"]] <- sample(seq_len(n_obs / 2L)) + (n_obs / 2L)
-      .d2[[2]][["tnum_b"]] <- sample(seq_len(n_obs / 2L))
+      .d2[[1]][["tnum_b"]] <- sample(seq_len(n_items / 2L)) + (n_items / 2L)
+      .d2[[2]][["tnum_b"]] <- sample(seq_len(n_items / 2L))
     }
     .d3 <- rbind(.d2[[1]], .d2[[2]])
     .d3[["Y_r"]] <- .d3[["Y_fit"]] + .e[.d3[["tnum_r"]]]
@@ -367,6 +353,9 @@ fit_2x2 <- function(dat, cs = FALSE, by_subj_fs = TRUE,
     names(v) <- rep(vn, 2)
     v
   }
+
+  stop("Need to update this function to account for item effects.\n",
+       "       By-item intercept and slope for B needed.")
   ## determine the model formula
   form_rhs <- "A_c * B_c + \n   s(subj_id, A_c, bs = \"re\")"
   form_rhs_b <- form_rhs # blocked version
@@ -463,6 +452,9 @@ mcsim <- function(nmc,
                     format(Sys.time(), "%Y-%m-%d-%H-%M-%S"),
                     Sys.info()[["nodename"]],
                     Sys.getpid())) {
+
+  stop("Need to update this function to account for item effects.\n",
+       "       By-item intercept and slope for B needed.")
   
   tfile <- tempfile(fileext = ".csv")
                     
@@ -499,114 +491,3 @@ mcsim <- function(nmc,
     invisible(ff)
   }
 }
-
-
-## sim_2x2 <- function(n_subj = 48, n_obs = 48, n_items = 48,
-##                     int = 0, A = 0, B = 0, AB = 0,
-##                     item_rint = .5, item_rslp = .5,
-##                     item_rcorr = .5, subj_rint = .5,
-##                     subj_rslp = .5, subj_rcorr = .5,
-##                     version = 0L,verbose = FALSE){
-
-##   if ((n_subj %% 4L) || (n_subj <= 0L))
-##     stop("'n_subj' must be a positive integer that is a multiple of 4")
-
-##   if ((n_obs %% 2L) || (n_obs <= 0L))
-##     stop("'n_obs' must be a positive even integer")
-
-##   if ((n_items %% 2L) || (n_items <= 0L))
-##     stop("'n_items' must be a positive even integer")
-
-##   if (n_obs %% n_items)
-##     warning("Are you sure 'n_items' shouldn't be a factor of 'n_obs'?")
-
-##   make_rfx <- function(n, rint, rslp, rcorr){
-##     rfx_mx <- matrix(c(rint,
-##                        sqrt(rint) * sqrt(rslp) * rcorr,
-##                        sqrt(rint) * sqrt(rslp) * rcorr,
-##                        rslp), nrow = 2)
-
-    
-##     rfx <- as.data.frame(
-##       MASS::mvrnorm(n,
-##                     c(rint = 0, rslp = 0),
-##                     rfx_mx))
-
-##     return(rfx)
-##   }  
-
-##   ## subj_rfx_mx <- make_rfx(subj_rint, subj_rslp, subj_rcorr)
-##   ## item_rfx_mx <- make_rfx(item_rint, item_rslp, item_rcorr)
-
-##   sfx <- make_rfx(n_subj, subj_rint, subj_rslp, subj_rcorr)
-##   ifx <- make_rfx(n_items, item_rint, item_rslp, item_rcorr)
-  
-##   ## sfx <- as.data.frame(
-##   ##   MASS::mvrnorm(n_subj,
-##   ##                 c(subj_rint = 0, subj_rslp = 0),
-##   ##                 subj_rfx_mx))
-
-##   ## ifx <- as.data.frame(
-##   ##   MASS::mvrnorm(n_items,
-##   ##                 c(item_rint = 0, item_rslp = 0),
-##   ##                 item_rfx_mx))
-
-
-##   sfx[["subj_id"]] <- factor(seq_len(n_subj))
-##   sfx[["B"]] <- factor(rep(c("B1", "B2"), times = n_subj / 2L))
-##   sfx[["blk_ord"]] <- factor(rep(rep(1:2, each = 2L), times = n_subj / 4L))
-
-##   ifx[["item_id"]] <- factor(seq_len(n_items))
-##   ifx[["A"]] <- factor(rep(c("A1", "A2"), times = n_items / 2L))
-
-##   ## Get complete list of all subject and item combinations
-##   trials <- expand.grid(
-##     subj_id = sfx[["subj_id"]],
-##     item_id = ifx[["item_id"]]
-##   )
-
-##   ## Join subject and item rfx
-##   trials_subj <- merge(sfx, trials, by = "subj_id")
-##   dat <- merge(ifx, trials_subj, by = "item_id")
-
-
-##   dat[["A_c"]] <- ifelse(dat[["A"]] == "A1", -.5, .5)
-##   dat[["B_c"]] <- ifelse(dat[["B"]] == "B1", -.5, .5)
-##   dat[["Y_fit"]] <-
-##     int + dat[["subj_rint"]] + dat[["item_rint"]] +
-##     (A + dat[["subj_rslp"]]) * dat[["A_c"]] +
-##     (B + dat[["item_rslp"]]) * dat[["B_c"]] +
-##     AB * dat[["A_c"]] * dat[["B_c"]]
-
-##   ds <- split(dat, dat[["subj_id"]])
-
-##   errs <- replicate(n_subj, errsim(n_obs, version), simplify = FALSE)
-
-##   ## add in the errors
-##   derr <- mapply(function(.d, .e) {
-##     .d[["tnum_r"]] <- sample(seq_len(nrow(.d)))
-##     .d2 <- split(.d, .d[["A"]])
-##     if (.d[["blk_ord"]][1] == 1L) {
-##       .d2[[1]][["tnum_b"]] <- sample(seq_len(n_obs / 2L))
-##       .d2[[2]][["tnum_b"]] <- sample(seq_len(n_obs / 2L)) + (n_obs / 2L)
-##     } else {
-##       .d2[[1]][["tnum_b"]] <- sample(seq_len(n_obs / 2L)) + (n_obs / 2L)
-##       .d2[[2]][["tnum_b"]] <- sample(seq_len(n_obs / 2L))
-##     }
-##     .d3 <- rbind(.d2[[1]], .d2[[2]])
-##     .d3[["Y_r"]] <- .d3[["Y_fit"]] + .e[.d3[["tnum_r"]]]
-##     .d3[["Y_b"]] <- .d3[["Y_fit"]] + .e[.d3[["tnum_b"]]]
-##     .d3
-##   }, ds, errs, SIMPLIFY = FALSE)
-
-##   dall <- do.call("rbind", derr)
-##   rownames(dall) <- NULL
-
-##   cols <- c("subj_id", "A", "B", "A_c", "B_c", "tnum_r", "tnum_b", "Y_r", "Y_b")
-
-##   if (verbose) {
-##     cols <- c(cols, "rint", "rslp", "Y_fit")
-##   }
-
-##   dall[, cols]
-## }
--- tests/testthat/test-sim-2x2.R
+++ tests/testthat/test-sim-2x2.R
@@ -8,32 +8,102 @@ test_that("randomization is correct", {
 })
 
 test_that("intercept is correct", {
-  dat <- sim_2x2(24, 24, 5, 0, 0, 0, 0, 0, 0, 1, TRUE)
+  dat <- sim_2x2(24, 24, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, TRUE)
   expect_equal(dat[["Y_fit"]],
                rep(5, length(dat[["Y_fit"]])))
 })
 
 test_that("A is correct", {
-  dat <- sim_2x2(24, 24, 0, 5, 0, 0, 0, 0, 0, 1, TRUE)
+  dat <- sim_2x2(24, 24, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 1, TRUE)
   agg <- aggregate(Y_fit ~ A, dat, mean)
   expect_equal(agg[["Y_fit"]][2] - agg[["Y_fit"]][1], 5)
 })
 
 test_that("B is correct", {
-  dat <- sim_2x2(24, 24, 0, 0, 5, 0, 0, 0, 0, 1, TRUE)
+  dat <- sim_2x2(24, 24, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 1, TRUE)
   agg <- aggregate(Y_fit ~ B, dat, mean)
   expect_equal(agg[["Y_fit"]][2] - agg[["Y_fit"]][1], 5)
 })
 
 test_that("AB is correct", {
-  dat <- sim_2x2(24, 24, 0, 0, 0, 5, 0, 0, 0, 1, TRUE)
+  dat <- sim_2x2(24, 24, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 1, TRUE)
   agg <- aggregate(Y_fit ~ A + B, dat, mean)[["Y_fit"]]
   expect_equal(agg[1] - agg[2] - (agg[3] - agg[4]), 5)
 })
 
+test_that("errs are correct", {
+  dat <- sim_2x2(24, 24, -1, 1, 2, 4, .5, .5, .5, .5, .5, .5, 0, TRUE)
+  dat[["err_r"]] <- with(dat, Y_r - Y_fit)
+  dat[["err_b"]] <- with(dat, Y_b - Y_fit)
+  dsplit <- split(dat, dat[["subj_id"]])
+  lv <- sapply(dsplit, function(.x) {
+    all(mapply(all.equal, sort(.x[["err_r"]]), sort(.x[["err_b"]])))
+  })
+  expect_true(all(lv))
+})
+
+test_that("A is within subject", {
+  dat <- sim_2x2()
+  xtbl <- xtabs(~subj_id + A, dat)
+  lv <- !sapply(seq_len(nrow(xtbl)), function(.x) {
+    any(sapply(xtbl[.x, ], function(.xx) {isTRUE(all.equal(.xx, 0))}))
+  })
+  expect_true(all(lv))
+})
+
+test_that("B is between subject", {
+  dat <- sim_2x2()
+  xtbl <- xtabs(~subj_id + B, dat)
+  lv <- sapply(seq_len(nrow(xtbl)), function(.x) {
+    any(sapply(xtbl[.x, ], function(.xx) {isTRUE(all.equal(.xx, 0))}))
+  })
+  expect_true(all(lv))
+})
+
+test_that("A is between item", {
+  dat <- sim_2x2()
+  xtbl <- xtabs(~item_id + A, dat)
+  lv <- sapply(seq_len(nrow(xtbl)), function(.x) {
+    any(sapply(xtbl[.x, ], function(.xx) {isTRUE(all.equal(.xx, 0))}))
+  })
+  expect_true(all(lv))
+})
+
+test_that("B is within item", {
+  dat <- sim_2x2()
+  xtbl <- xtabs(~item_id + B, dat)
+  lv <- !sapply(seq_len(nrow(xtbl)), function(.x) {
+    any(sapply(xtbl[.x, ], function(.xx) {isTRUE(all.equal(.xx, 0))}))
+  })
+  expect_true(all(lv))
+})
+
+test_that("A is between subject", {
+  dat <- sim_2x2()
+  xtbl <- xtabs(~subj_id + B, dat)
+  expect_equal(c(48L, 2L), dim(xtbl))
+})
+
+test_that("fitted values are correct", {
+  int <- -1
+  A_eff <- 1
+  B_eff <- 2
+  AB_eff <- 4
+  dat <- sim_2x2(24, 24, int, A_eff, B_eff, AB_eff, .5, .5, .5, .5, .5, .5, 0, TRUE)
+  dat[["fits"]] <- with(dat,
+                        int + subj_rint + item_rint +
+                        (A_eff + subj_rslp) * A_c +
+                        (B_eff + item_rslp) * B_c +
+                        AB_eff * A_c * B_c)
+  lv <- mapply(function(.x, .y) {
+    isTRUE(all.equal(.x, .y))},
+    dat[["Y_fit"]], dat[["fits"]])
+  expect_true(all(lv))
+})
+
 test_that("all residual SDs are 1", {
-  ff <- lapply(1:9, function(.i) {
-    dat <- sim_2x2(12, 12, 0, 0, 0, 0, 1, 1, .5, .i, TRUE)
+  ff <- lapply(0:8, function(.i) {
+    dat <- sim_2x2(12, 12, 0, 0, 0, 0, 1, 1, .5, 0, 0, 0, .i, TRUE)
     err <- dat[["Y_r"]] - dat[["Y_fit"]]
     ds <- split(err, dat[["subj_id"]])
     vv <- sapply(ds, sd)
@@ -46,8 +116,8 @@ test_that("all residual SDs are 1", {
 })
 
 test_that("all residual means are 1", {
-  ff <- lapply(1:9, function(.i) {
-    dat <- sim_2x2(12, 12, 0, 0, 0, 0, 1, 1, .5, .i, TRUE)
+  ff <- lapply(0:8, function(.i) {
+    dat <- sim_2x2(12, 12, 0, 0, 0, 0, 1, 1, .5, 0, 0, 0, .i, TRUE)
     err <- dat[["Y_r"]] - dat[["Y_fit"]]
     ds <- split(err, dat[["subj_id"]])
     vv <- sapply(ds, mean)
@@ -57,4 +127,8 @@ test_that("all residual means are 1", {
   expect_true(all(sapply(ff, function(.x) {
     all.equal(.x, rep(0, 12), tolerance = 1e-10)
   })))
+})
+
+test_that("item variance is correct", {
+  expect_true(TRUE)
 })
